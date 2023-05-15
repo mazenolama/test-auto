43c6dd113198274ea90ff1e3fd989fd8006c7c0c
mazenolama <mazen.olama00@gmail.com>
2023-05-15 12:38:02 +0300

    2nd

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
 <?php
+
+// Check if the request method is POST
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Get the request payload
+    $payload = file_get_contents('php://input');
+
+    // Write the payload to a text file
+    file_put_contents('payload.txt', $payload);
+
+    // Send a response
+    http_response_code(200);
+    echo 'Payload written to file.';
+} else {
+    // Send an error response for unsupported request methods
+    http_response_code(405);
+    echo 'Method Not Allowed.';
+}
+
+/*
 // Get the incoming webhook notification payload
 $jsonPayload = file_get_contents('php://input');
 
@@ -12,7 +31,7 @@ if (isset($payload['ref']) && $payload['ref'] === 'refs/heads/main') {
   $output = shell_exec('cd H:/Workspace/Hadef IT/Local-QM System && git pull');
 
   // Log the output of the Git pull command
-  file_put_contents('H:/Workspace/Hadef IT/Local-QM System/auto-pull/logfile.txt', $output, FILE_APPEND);
+  file_put_contents('logfile.txt', $output, FILE_APPEND);
 
   // Respond to the webhook notification with a success message
   http_response_code(200);
@@ -25,3 +44,4 @@ if (isset($payload['ref']) && $payload['ref'] === 'refs/heads/main') {
   echo 'Invalid webhook notification payload';
 
 }
+-*/
