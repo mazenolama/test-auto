d1885bcd33d35f3117cf6624d6d84b2921776e77
mazenolama <mazen.olama00@gmail.com>
2023-05-15 13:12:33 +0300

    adaw

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ const git = require('simple-git')();
 
 const PORT = 4000;
 const REPO_PATH = 'H:\\Workspace\\Hadef IT\\Local-QM System\\testpull\\test-auto';
-
+console.log("testsssssssssss");
 app.post('/', (req, res) => {
-console.log(res)
   git.cwd(REPO_PATH)
     .pull((err, update) => {
       if (err) {
