f26818a06b63bd94cbbeb34ca6ea2b39c366ad54
mazenolama <mazen.olama00@gmail.com>
2023-05-15 12:52:14 +0300

    d

--- index.php
+++ index.php
@@ -4,28 +4,9 @@
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Get the incoming webhook notification payload
     $payload = file_get_contents('php://input');
-    // Parse the JSON payload into an associative array
-    // Decode the JSON payload
-    $data = json_decode($payload, true);
-    file_put_contents('log.json', $payload, FILE_APPEND);
-
-    // Check if decoding was successful
-    if ($data !== null) {
-        // Access the decoded payload data
-        $eventName = $data['event_name'];
-        $repositoryName = $data['repository']['name'];
-
-        // Perform further processing with the decoded data
-        // ...
-    } else {
-        // Handle decoding failure
-        http_response_code(400);
-        echo 'Invalid JSON payload.';
-    }
 
     // Check if the payload is for a push event
-   /* if (isset($payload['ref']) && $payload['ref'] === 'refs/heads/main') {
-
+    if (isset($payload['ref']) && $payload['ref'] === 'refs/heads/main') {
         // Execute the Git pull command to update the repository
         $output = shell_exec('cd H:\Workspace\Hadef IT\Local-QM System\testpull System && git pull');
 
@@ -42,7 +23,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         http_response_code(400);
         echo 'Invalid webhook notification payload';
 
-    }*/
+    }
 
 } else {
     // Send an error response for unsupported request methods
