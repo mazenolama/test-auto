910b09bdf8cd2335331a9bd0d657434eb014edae
mazenolama <mazen.olama00@gmail.com>
2023-05-15 12:48:15 +0300

    d

--- index.php
+++ index.php
@@ -3,13 +3,28 @@
 // Check if the request method is POST
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Get the incoming webhook notification payload
-    $jsonPayload = file_get_contents('php://input');
+    $payload = file_get_contents('php://input');
     // Parse the JSON payload into an associative array
-    $payload = json_decode($jsonPayload);
-    file_put_contents('logfile.json', $jsonPayload, FILE_APPEND);
+    // Decode the JSON payload
+    $data = json_decode($payload, true);
+    file_put_contents('logfile.json', $data, FILE_APPEND);
+
+    // Check if decoding was successful
+    if ($data !== null) {
+        // Access the decoded payload data
+        $eventName = $data['event_name'];
+        $repositoryName = $data['repository']['name'];
+
+        // Perform further processing with the decoded data
+        // ...
+    } else {
+        // Handle decoding failure
+        http_response_code(400);
+        echo 'Invalid JSON payload.';
+    }
 
     // Check if the payload is for a push event
-    if (isset($payload['ref']) && $payload['ref'] === 'refs/heads/main') {
+   /* if (isset($payload['ref']) && $payload['ref'] === 'refs/heads/main') {
 
         // Execute the Git pull command to update the repository
         $output = shell_exec('cd H:\Workspace\Hadef IT\Local-QM System\testpull System && git pull');
@@ -27,7 +42,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         http_response_code(400);
         echo 'Invalid webhook notification payload';
 
-    }
+    }*/
 
 } else {
     // Send an error response for unsupported request methods
